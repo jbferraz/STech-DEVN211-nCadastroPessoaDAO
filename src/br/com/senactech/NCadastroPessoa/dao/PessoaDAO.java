a0189ba06e08f7f67cbf8125482087546ee264b2
Jair Borges Ferraz <jbferraz@senacinformatica.br>
2022-06-20 21:09:19 -0300

    CRUD do PessoaDAO completo

--- src/br/com/senactech/NCadastroPessoa/dao/PessoaDAO.java
+++ src/br/com/senactech/NCadastroPessoa/dao/PessoaDAO.java
@@ -80,4 +80,108 @@ public class PessoaDAO {
             stat.close();
         }
     }
+    
+    public boolean verCPF(String cpf) throws SQLException{
+        //Busca conexão do BD
+        Connection con = Conexao.getConexao();
+        //cria espaço de trabalho sql, é a área no Java onde 
+        //vamos executar os scripts SQL
+        Statement stat = con.createStatement();
+        
+        boolean verCPF = false;
+        
+        try {
+            String sql;
+            sql = "select cpf from pessoa where cpf = " + cpf;
+            ResultSet rs = stat.executeQuery(sql);
+            while (rs.next()) {                
+                verCPF = rs.wasNull();
+            }
+        } catch (SQLException e) {
+            throw new SQLException("Pessoa com este CPF não existe! \n"
+                + e.getMessage());
+        } finally {
+            con.close();
+            stat.close();
+        }
+        return verCPF;
+    }
+    
+    public Pessoa getByDoc(String cpf) throws SQLException{
+        //Busca conexão do BD
+        Connection con = Conexao.getConexao();
+        //cria espaço de trabalho sql, é a área no Java onde 
+        //vamos executar os scripts SQL
+        Statement stat = con.createStatement();
+        Pessoa p = new Pessoa();
+        
+        try {
+            String sql;
+            sql = "select * from pessoa where cpf = " + cpf;
+            ResultSet rs = stat.executeQuery(sql);
+            while (rs.next()) {                
+                //lado do java |x| lado do banco
+                p.setIdPessoa(rs.getInt("idPessoa"));
+                p.setNomePessoa(rs.getString("nomePessoa"));
+                p.setCpf(rs.getString("cpf"));
+                p.setEndereco(rs.getString("endereco"));
+                p.setTelefone(rs.getString("telefone"));
+                p.setIdade(rs.getInt("idade"));
+                p.setStatus(rs.getBoolean("status"));
+            }
+        } catch (SQLException e) {
+            throw new SQLException("Pessoa com este cpf não existe!\n"
+                + e.getMessage());
+        } finally {
+            con.close();
+            stat.close();
+        }
+        return p;
+    }
+    
+    public void deletarPessoa(int id) throws SQLException{
+        //Busca conexão do BD
+        Connection con = Conexao.getConexao();
+        //cria espaço de trabalho sql, é a área no Java onde 
+        //vamos executar os scripts SQL
+        Statement stat = con.createStatement();
+        
+        try {
+            String sql;
+            sql = "delete from pessoa where idPessoa = " + id;
+            stat.execute(sql);
+        } catch (SQLException e) {
+            throw new SQLException("Erro ao deletar Pessoa. \n"
+                + e.getMessage());
+        } finally {
+            con.close();
+            stat.close();
+        }
+    }
+    
+    public void atualizarPessoa (Pessoa pVO) throws SQLException{
+        //Busca conexão do BD
+        Connection con = Conexao.getConexao();
+        //cria espaço de trabalho sql, é a área no Java onde 
+        //vamos executar os scripts SQL
+        Statement stat = con.createStatement();
+        
+        try {
+            String sql;
+            sql = "update pessoa set "
+                    + "nomePessoa = '" + pVO.getNomePessoa() + "', "
+                    + "endereco = '" + pVO.getEndereco() + "', "
+                    + "idade = " + pVO.getIdade() + ", "
+                    + "telefone = '" + pVO.getTelefone() + "', "
+                    + "status = " + pVO.isStatus() + " "
+                    + "where idPessoa = " + pVO.getIdPessoa();
+            stat.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new SQLException("Erro ao atualizar Pessoa. \n"
+                +e.getMessage());
+        } finally {
+            con.close();
+            stat.close();
+        }
+    }
 }
